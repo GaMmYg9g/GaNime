217c150b43862166c542084eabc83dacaa61531a
GaMmYg9g <gammytheninth@gmail.com>
2026-08-20 14:52:59 -0400

    Update data4.js

--- js/data/data4.js
+++ js/data/data4.js
@@ -1591,7 +1591,7 @@ const ANIMES_PARTE3 = [
     nombre: "Monster",
     estado: "en-descarga",
     capitulosTotales: 74,
-    capitulosActuales: 12,
+    capitulosActuales: 20,
     portada: "IMG/monster.jpg",
     temporadasTotales: 1,
     temporadaActual: 1,
